d5c6ef16f68d937409e23fcf408513250996ce5b
Najdan Tomic <90030627+najdan-liveramp@users.noreply.github.com>
2022-12-05 14:29:15 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+        name: "LRPrivacyManagerSDK",
+        platforms: [.iOS(.v10)],
+        products: [
+            .library(
+                    name: "LRPrivacyManagerSDK",
+                    targets: ["LRPrivacyManager"]
+            ),
+        ],
+        dependencies: [
+            .package(url: "https://github.com/liveramp/pl-sdk-ios-acc", from: "1.0.7")
+        ],
+        targets: [
+            .binaryTarget(
+                name: "LRPrivacyManagerSDK", url: "https://gdpr-sdk-ios-prod.launch.liveramp.com/1.1.12/LRPrivacyManagerSDK.zip", checksum: "1afe93d4cee84223de5013e968ee27f0d981944bacecb9dac8dac53d88dad180"
+            ),
+            .target(
+                name: "LRPrivacyManager",
+                dependencies: [
+                    .target(name: "LRPrivacyManagerSDK"),
+                    .product(name: "LRPreferenceLinkSDK", package: "pl-sdk-ios-acc")
+                ],
+                path: "Sources"
+            )
+        ]
+)
